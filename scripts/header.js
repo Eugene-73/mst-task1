6fadad9a59472b8daa7804a8c2edaa1297a4f6b3
Eugene <zhenel1998@yandex.ru>
2025-01-21 21:56:04 +0400

    first commit

---
+++ scripts/header.js
@@ -0,0 +1,8 @@
+const header = document.getElementsByClassName("header")[0];
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 40) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
